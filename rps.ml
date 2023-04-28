8e3ee3fc0c298560b7f60c64b9b825102a6c970b
Kyle Zhang <zkyle1337@gmail.com>
2023-04-27 21:00:52 -0700

    refactor with records

--- rps.ml
+++ rps.ml
@@ -1,5 +1,17 @@
+open Printf
+
 type choice = Rock | Paper | Scissors
 type result = Win | Loss | Tie
+
+type player = {
+  strategy : choice list -> choice list -> choice;
+  name : string;
+  log : choice list;
+  health : int;
+}
+
+let string_of_result res =
+  match res with Win -> "Win" | Loss -> "Loss" | Tie -> "Tie"
 
 let match_rps rock_outcome paper_outcome scissors_outcome choice =
   match choice with
@@ -7,42 +19,45 @@ let match_rps rock_outcome paper_outcome scissors_outcome choice =
   | Paper -> paper_outcome
   | Scissors -> scissors_outcome
 
+let string_of_rps = match_rps "Rock" "Paper" "Scissors"
 let counter_to = match_rps Paper Scissors Rock
 let beaten_by = match_rps Scissors Rock Paper
 let damage = match_rps 1 1 1
-let strategyRock _ = Rock
-let strategyPaper _ = Paper
+let strategyRock _ _ = Rock
+let strategyPaper _ _ = Paper
+let strategyCounter log _ = match log with [ h ] -> counter_to h | _ -> Rock
 
-let make_choice strategy1 strategy2 log =
-  let choice1 = strategy1 log in
-  let choice2 = strategy2 log in
+let make_choice strategy1 strategy2 log1 log2 =
+  let choice1 = strategy1 log1 log2 in
+  let choice2 = strategy2 log2 log1 in
   (choice1, choice2)
 
 (**
-    returns the winner of two given strategies, Win iff strategy1 wins
+    returns the winner of two given players, Win iff player1 wins
 *)
-let rec winning_strategy ?(health1 = 100) ?(health2 = 100) ?(log = []) strategy1
-    strategy2 =
-  if health1 = 0 && health2 = 0 then Tie
-  else if health1 <= 0 then Loss
-  else if health2 <= 0 then Win
+let rec do_battle ?(debug = false) player1 player2 =
+  if player1.health = 0 && player2.health = 0 then Tie
+  else if player1.health <= 0 then Loss
+  else if player2.health <= 0 then Win
   else
-    let choice1, choice2 = make_choice strategy1 strategy2 log in
+    let choice1, choice2 =
+      make_choice player1.strategy player2.strategy player1.log player2.log
+    in
+    let _ =
+      if debug then
+        printf "%s picks: %s, %s picks: %s\n" player1.name
+          (string_of_rps choice1) player2.name (string_of_rps choice2)
+      else ()
+    in
     if choice1 = beaten_by choice2 then
-      winning_strategy
-        ~health1:(health1 - damage choice2)
-        ~health2
-        ~log:((choice1, choice2) :: log)
-        strategy1 strategy2
+      do_battle ~debug
+        { player1 with health = player1.health - damage choice2 }
+        player2
     else if choice1 = counter_to choice2 then
-      winning_strategy ~health1
-        ~health2:(health2 - damage choice1)
-        ~log:((choice1, choice2) :: log)
-        strategy1 strategy2
+      do_battle ~debug player1
+        { player2 with health = player2.health - damage choice1 }
     else
       (* both players take damage in event of a tie to prevent infinite recursion *)
-      winning_strategy
-        ~health1:(health1 - damage choice2)
-        ~health2:(health2 - damage choice1)
-        ~log:((choice1, choice2) :: log)
-        strategy1 strategy2
+      do_battle ~debug
+        { player1 with health = player1.health - damage choice2 }
+        { player2 with health = player2.health - damage choice1 }
--- test.ml
+++ test.ml
@@ -1,15 +1,30 @@
 open OUnit2
 open Rps
 
+let make_player strategy name = { strategy; name; log = []; health = 100 }
+let playerPaper = make_player strategyPaper "paper only"
+let playerRock = make_player strategyRock "rock only"
+let playerCounter = make_player strategyCounter "counterpick"
+
 let tests =
   "test suite for rps"
   >::: [
          ( "paper beats rock" >:: fun _ ->
-           assert_equal Win (winning_strategy strategyPaper strategyRock) );
+           assert_equal Win
+             (do_battle playerPaper playerRock)
+             ~printer:string_of_result );
          ( "rock loses to paper" >:: fun _ ->
-           assert_equal Loss (winning_strategy strategyRock strategyPaper) );
+           assert_equal Loss
+             (do_battle playerRock playerPaper)
+             ~printer:string_of_result );
          ( "rock ties to rock" >:: fun _ ->
-           assert_equal Tie (winning_strategy strategyRock strategyRock) );
+           assert_equal Tie
+             (do_battle playerRock playerRock)
+             ~printer:string_of_result );
+         ( "counterpick beats rock" >:: fun _ ->
+           assert_equal Win
+             (do_battle ~debug:true playerCounter playerRock)
+             ~printer:string_of_result );
        ]
 
 let _ = run_test_tt_main tests
